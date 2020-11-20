bae21e9207154680290091d5539f0a48649dcb10
Bruno de Sousa pagno <51502023+bruno-pagno@users.noreply.github.com>
2020-11-19 21:43:28 -0300

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+all:
+	gcc problema_escritores.c -o problema_escritores
+run:
+	./problema_escritores
---
+++ problema_escritores.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf("Problema dos escritores\n");
+    return 0;
+}
